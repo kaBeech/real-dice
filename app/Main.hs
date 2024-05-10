31e5d50dfcf707a8ad1d00c8023b879b926f8d8f
kaBeech <contact@kaBeech.com>
2024-05-09 23:10:57 -0700

    Add pseudorandom int list

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import BinaryString qualified (binaryString)
+-- import Pseudorandom qualified (pseudorandomizedIntArray)
 
 main :: IO ()
 main = do
+  -- print Pseudorandom.pseudorandomizedIntArray
   putStrLn "Generating data files..."
   writeFile "generated-data/raw_binary_data.dat" BinaryString.binaryString
