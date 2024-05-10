d055cb13967d6acb764c440a9423fea0b677c297
kaBeech <contact@kaBeech.com>
2024-05-09 21:43:11 -0700

    Cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,8 @@
 module Main where
 
 import BinaryString qualified (binaryString)
-import MyLib qualified (someFunc)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
   putStrLn BinaryString.binaryString
-  MyLib.someFunc
