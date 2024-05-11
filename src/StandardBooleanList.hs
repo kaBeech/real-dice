03b52875ca6989960013e9ad3aeee0c95dc30ae6
kaBeech <contact@kaBeech.com>
2024-05-10 23:19:28 -0700

    Cleanup

--- src/StandardBooleanList.hs
+++
@@ -1,6 +0,0 @@
-module StandardBooleanList (standardBooleanList) where
-
-import Pseudorandom qualified (pseudorandomizedIntList)
-
-standardBooleanList :: [Bool]
-standardBooleanList = map even Pseudorandom.pseudorandomizedIntList
