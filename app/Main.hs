9a56f11ccdfd1e6a7285c9eb9c39fc5c34995264
kaBeech <contact@kaBeech.com>
2024-05-12 03:07:19 -0700

    Clarify raw naming and add full length lists

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
 module Main where
 
-import RawDataCollection qualified (binaryStringFullLength, binaryStringPrimeLength)
+-- import Pseudorandom qualified (pseudorandomizedIntListFullLength, pseudorandomizedIntListPrimeLength)
+import RawDataCollection qualified (rawBinaryStringFullLength, rawBinaryStringPrimeLength)
 
--- import Pseudorandom qualified (pseudorandomizedIntArray)
 -- import StandardBooleanList qualified (standardBooleanList)
 
 main :: IO ()
 main = do
-  -- print Pseudorandom.pseudorandomizedIntArray
+  -- print Pseudorandom.pseudorandomizedIntListPrimeLength
+  -- print Pseudorandom.pseudorandomizedIntListFullLength
   putStrLn "Generating data files..."
-  writeFile "generated-data/raw_binary_data_full_length.dat" RawDataCollection.binaryStringFullLength
-  writeFile "generated-data/raw_binary_data_prime_length.dat" RawDataCollection.binaryStringPrimeLength
+  writeFile "generated-data/raw_binary_data_full_length.dat" RawDataCollection.rawBinaryStringFullLength
+  writeFile "generated-data/raw_binary_data_prime_length.dat" RawDataCollection.rawBinaryStringPrimeLength
 
 -- writeFile "generated-data/standard_boolean_list.dat" (show StandardBooleanList.standardBooleanList)
