f313604073f9bb5ec3263cd8a85ded6641e3c81e
kaBeech <contact@kaBeech.com>
2024-05-14 13:55:18 -0700

    Export RNG class

--- src/RealDice/RNG.hs
+++ src/RealDice/RNG.hs
@@ -1,18 +1,18 @@
-module RealDice.RNG (randomInt, randomFloat, randomDouble, makeRNG, makeRNGCustom) where
+module RealDice.RNG (RNG, randomInt, randomFloat, randomDouble, makeRNG, makeRNGCustom) where
 
 import Control.Monad.State
 import RealDice.Generate.StandardRNGTables (standardTableIntPrimeLength)
 import RealDice.Manipulate.GetValueFromRNGTable (getIntByIndex)
 import RealDice.Manipulate.RandomizeList (randomizeList)
 
-data RNGState where
-  RNGState :: {index :: Int, rngTable :: [Int]} -> RNGState
+data RNG where
+  RNG :: {index :: Int, rngTable :: [Int]} -> RNG
 
-makeRNG :: Int -> RNGState
+makeRNG :: Int -> RNG
 makeRNG i = makeRNGCustom i standardTableIntPrimeLength
 
-makeRNGCustom :: Int -> [Int] -> RNGState
-makeRNGCustom i table = RNGState {index = i, rngTable = table}
+makeRNGCustom :: Int -> [Int] -> RNG
+makeRNGCustom i table = RNG {index = i, rngTable = table}
 
 -- _exampleRandomInt :: [Int]
 -- _exampleRandomInt = do
@@ -21,7 +21,7 @@ makeRNGCustom i table = RNGState {index = i, rngTable = table}
 --   let randomIntResult2 = evalState (randomInt (-99) 99) rngState'
 --   [randomIntResult1, randomIntResult2]
 
-randomInt :: Int -> Int -> State RNGState Int
+randomInt :: Int -> Int -> State RNG Int
 randomInt minResult maxResult = do
   rng <- get
   let rngIndex = index rng
@@ -29,13 +29,13 @@ randomInt minResult maxResult = do
   let possibleResults = randomizeList [minResult .. maxResult]
   let table = rngTable rng
   let resultIndex = getIntByIndex rngIndex table
-  put RNGState {index = rngIndex', rngTable = table}
+  put RNG {index = rngIndex', rngTable = table}
   return (getIntByIndex resultIndex possibleResults)
 
-randomFloat :: Int -> State RNGState Float
+randomFloat :: Int -> State RNG Float
 randomFloat decimalPrecision = randomFloatPerDecimal decimalPrecision 0
 
-randomFloatPerDecimal :: Int -> Float -> State RNGState Float
+randomFloatPerDecimal :: Int -> Float -> State RNG Float
 randomFloatPerDecimal 0 currentFloat = do
   return currentFloat
 randomFloatPerDecimal decimalPlace currentFloat = do
@@ -48,13 +48,13 @@ randomFloatPerDecimal decimalPlace currentFloat = do
   let digit = getIntByIndex resultIndex possibleResults
   let decimalPlace' = decimalPlace - 1
   let currentFloat' = currentFloat + (fromIntegral digit / (10 ^ decimalPlace))
-  put RNGState {index = rngIndex', rngTable = table}
+  put RNG {index = rngIndex', rngTable = table}
   randomFloatPerDecimal decimalPlace' currentFloat'
 
-randomDouble :: Int -> State RNGState Double
+randomDouble :: Int -> State RNG Double
 randomDouble decimalPrecision = randomDoublePerDecimal decimalPrecision 0
 
-randomDoublePerDecimal :: Int -> Double -> State RNGState Double
+randomDoublePerDecimal :: Int -> Double -> State RNG Double
 randomDoublePerDecimal 0 currentDouble = do
   return currentDouble
 randomDoublePerDecimal decimalPlace currentDouble = do
@@ -67,5 +67,5 @@ randomDoublePerDecimal decimalPlace currentDouble = do
   let digit = getIntByIndex resultIndex possibleResults
   let decimalPlace' = decimalPlace - 1
   let currentDouble' = currentDouble + (fromIntegral digit / (10 ^ decimalPlace))
-  put RNGState {index = rngIndex', rngTable = table}
+  put RNG {index = rngIndex', rngTable = table}
   randomDoublePerDecimal decimalPlace' currentDouble'
