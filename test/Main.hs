85300e6ec9181be9e53056f90bc89345ab0c054a
kaBeech <contact@kaBeech.com>
2024-05-14 18:39:33 -0700

    Add CoinSpec

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
 import Control.Monad qualified
+import RealDice.CoinSpec (doesnt_give_same_result_10_times)
+import RealDice.RNGSpec (returns_many_unique_numbers)
 import RealDice.Util.PrimeSpec (next_largest_prime_is_larger_than_input, result_is_less_than_or_equal_to_input, result_is_prime)
 import System.Exit
 import Test.QuickCheck
@@ -16,6 +18,14 @@ main = do
   check result_is_less_than_or_equal_to_input
   putStrLn "...returns the largest possible result:"
   check next_largest_prime_is_larger_than_input
+  putStrLn "Testing RealDice.CoinSpec..."
+  putStrLn "..flipCoin..."
+  putStrLn "...doesn't give the same result 10 times in a row:"
+  check doesnt_give_same_result_10_times
+  putStrLn "Testing RealDice.RNGSpec..."
+  putStrLn "..randomIntR..."
+  putStrLn "...returns many unique numbers:"
+  check returns_many_unique_numbers
   putStrLn "All tests pass!"
 
 isPass :: Result -> Bool
