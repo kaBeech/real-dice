dcaf9bdf2250291b91a4d4d8f94ea65ad8ca00d5
kaBeech <contact@kaBeech.com>
2024-05-15 11:50:55 -0700

    Add documentation

--- src/RealDice/Convert/CSV.hs
+++ src/RealDice/Convert/CSV.hs
@@ -4,8 +4,17 @@ module RealDice.Convert.CSV
   )
 where
 
+-- | Remove the square brackets ([]) from the string representations of a list so it can be used as a CSV string.
+-- For Example:
+-- >>> intsToCSV [1,2,3]
+-- "1,2,3"
 intsToCSV :: [Int] -> String
 intsToCSV xs = tail (take (length (show xs) - 1) (show xs))
 
-csvToInts :: String -> [Int]
+-- | Add square brackets ([]) to a CSV string of integers and make it into a list. Example: "1,2,3" -> [1,2,3]
+csvToInts ::
+  -- | This had better be a CSV of integers...
+  String ->
+  -- | ...because it's gonna be parsed into a list of integers!
+  [Int]
 csvToInts xs = read ("[" ++ xs ++ "]")
