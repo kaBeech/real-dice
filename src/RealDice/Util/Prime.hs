ead82ebe381baed3ec8ef7036556e3814f19cdd7
kaBeech <contact@kaBeech.com>
2024-05-15 00:46:35 -0700

    Add guards for low numbers

--- src/RealDice/Util/Prime.hs
+++ src/RealDice/Util/Prime.hs
@@ -3,4 +3,8 @@ module RealDice.Util.Prime (greatestPrimeNotGreaterThan) where
 import Data.Numbers.Primes (isPrime)
 
 greatestPrimeNotGreaterThan :: Int -> Int
-greatestPrimeNotGreaterThan n = head $ filter isPrime [n, n - 1 ..]
+greatestPrimeNotGreaterThan n
+  | n < 1 = 0
+  | n == 1 = 1
+  | isPrime n = n
+  | otherwise = greatestPrimeNotGreaterThan (n - 1)
