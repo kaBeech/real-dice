2828c2d0ee4dede4d8ac35ca6bae648353c95473
kaBeech <contact@kaBeech.com>
2024-05-15 00:57:58 -0700

    Add tests

--- test/RealDice/Util/PrimeSpec.hs
+++ test/RealDice/Util/PrimeSpec.hs
@@ -5,6 +5,8 @@ module RealDice.Util.PrimeSpec
   ( result_is_less_than_or_equal_to_input,
     result_is_prime,
     next_largest_prime_is_larger_than_input,
+    inputs_lower_than_1_return_0,
+    result_equals_result_from_equivalnt_function,
   )
 where
 
@@ -23,3 +25,9 @@ next_largest_prime_is_larger_than_input :: Int -> Property
 next_largest_prime_is_larger_than_input n = do
   let result = greatestPrimeNotGreaterThan n
   n > 1 ==> n < head (filter isPrime [result + 1, result + 2 ..])
+
+inputs_lower_than_1_return_0 :: Int -> Property
+inputs_lower_than_1_return_0 n = n < 1 ==> greatestPrimeNotGreaterThan n == 0
+
+result_equals_result_from_equivalnt_function :: Int -> Property
+result_equals_result_from_equivalnt_function n = n > 1 ==> greatestPrimeNotGreaterThan n == head (filter isPrime [n, n - 1 ..])
